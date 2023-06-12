ebd8e84748e31dc379983aa700f94ed38b20652e
Lore1ei <harubator@gmail.com>
2023-06-12 19:07:23 +0700

    customers get Request

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe, Req, Res } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe, Req, Res } from "@nestjs/common";
 import { CustomersService } from "../../services/customers/customers.service";
 import { Request, Response } from "express";
 
@@ -14,6 +14,19 @@ export class CustomersController {
     @Req() req: Request,
     @Res() res: Response){
     console.log(typeof id)
-    // return this.customersService.findCustomerById(id)
+      const customer = this.customersService.findCustomerById(id);
+    if(customer){
+      res.send(customer)
+    }else{
+      res.status(400).send({msg: 'customer not found!'})
+    }
+  }
+
+  @Get('/search/:id')
+  searchCustomerById(
+    @Param('id', ParseIntPipe) id: number){
+      const customer = this.customersService.findCustomerById(id);
+      if(customer) return customer;
+      throw new HttpException('Customer not found', HttpStatus.BAD_REQUEST)
   }
 }
