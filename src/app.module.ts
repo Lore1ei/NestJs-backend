b8ade860ee752f80b57e66e4921e3424566633f6
Lore1ei <harubator@gmail.com>
2023-06-17 10:24:32 +0700

    start working with psql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [CustomersModule, UsersModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'admin',
+    database: 'nest_postgres',
+    entities: [],
+    synchronize: true,
+  })],
   controllers: [],
   providers: [],
 })
